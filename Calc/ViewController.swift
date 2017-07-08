82bdc20fd3f3396ef619b5e71c87128666f38f24
hisham <m.hisham@live.com>
2017-07-08 20:06:54 +0200

    testing XCODE sourcecontrol sys

--- Calc/ViewController.swift
+++ Calc/ViewController.swift
@@ -10,16 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+    
+    
+    
+  //underscor before the name means that there is no extenal variable 
+    @IBAction func touchDigit(_ sender: UIButton) {
+        
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+//    func drawHorizontalLine ( from posX: Double , to posY: Double , using color: UIColor){}
+    
+    
 }
 
